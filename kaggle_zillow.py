f66e0d9eef23d6c5f5ae98ac5905370eb8c732cf
Adrian Albert <adalbert@ocio-gpu01.slac.stanford.edu>
2017-07-12 09:04:55 -0700

    initial working pipeline

---
+++ kaggle_zillow.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+
+import calendar
+
+import gc
+
+class Zillow(Object):
+	def __init__(self):
+		pass
+
+	def load_data(self):
+		pass
+
+	def train(self):
+		pass
+
+	def format_submission(self):
+		pass
+
+
+def format_data():
+	pass
